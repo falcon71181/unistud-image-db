c9c5e47131d80bca2a1776b3c5a272af56066845
Dev <flashytesting@gmail.com>
2023-09-11 01:49:01 +0530

    Create html file for found images

--- main.py
+++ main.py
@@ -2,12 +2,19 @@ from urllib.request import urlopen
 import os
 
 URL = "https://punjab.chitkara.edu.in/Storage/Images/Student/";
+INDEX_URL = f"file://{os.getcwd()}/index.html"
+
 START = 53675
 END = 53725
+SIZE = (END - START) + 1
+
+BOILERPLATE_START = '''<DOCKTYPE html><html><head><title>Images</title>
+<link href='./style.css' rel='stylesheet' /></head><body><div class='container'>'''
+BOILERPLATE_END = "<div></body></html>"
 
 def scrap(num):
-    value = (num - START) + 1
-    percentage = (value / length) * 100
+    relative_value = (num - START) + 1
+    percentage = (relative_value / SIZE) * 100
 
     print(f"Status: {round(percentage, 2)}%");
 
@@ -21,21 +28,22 @@ def scrap(num):
 
     return image
 
-
 def main():
-    image_file = open("./images.db", "w")
-    length = END - START
+    images_section = ""
 
-    for num in range(START, END):
+    for num in range(START, END + 1):
         os.system('clear')
         print(f"Extracting images from {START} - {END}")
-        image = scrap(num)
 
-        if (image != -1):
-            image_file.write(f"{image} \n")
+        image_value = scrap(num)
 
-    print(f"Extration completed")
-    image_file.close();
+        if (image_value != -1):
+            images_section += f"<img src='{URL}/{image_value}.jpg' />"
+
+    with open("./index.html", "w") as image_file:
+        image_file.write(BOILERPLATE_START + images_section + BOILERPLATE_END)
+
+    print(f"Extration completed. Open: {INDEX_URL}")
 
 
 if __name__ == "__main__":
