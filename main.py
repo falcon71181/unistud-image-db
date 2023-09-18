2f6310934cbd6aef21c974d744529b25a950c33f
Dev <96491001+BugReportOnWeb@users.noreply.github.com>
2023-09-18 22:08:49 +0530

    Remove clear_screen function (redundant)

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os
+mport os
 import requests
 from jinja2 import Environment, FileSystemLoader
 from concurrent.futures import ThreadPoolExecutor
@@ -10,25 +10,26 @@ START = 53675
 END = 53725
 
 MAX_THREADS = 300
+
 def scrap_image(num):
     main_url = f"{URL}{num}.jpg"
+
     try:
         response = requests.get(main_url)
         if response.status_code == 200:
             return num
     except requests.exceptions.RequestException:
         pass
+
     return -1
 
 def create_html_page(images):
     env = Environment(loader=FileSystemLoader('.'))
     template = env.get_template('template.html')
     html_content = template.render(images=images)
+
     with open('index.html', 'w') as html_file:
         html_file.write(html_content)
-
-def clear_screen():
-    os.system('clear' if os.name == 'posix' else 'cls')
 
 def main():
     images = []
@@ -36,7 +37,6 @@ def main():
     with ThreadPoolExecutor(max_workers=MAX_THREADS) as executor:
         futures = [executor.submit(scrap_image, num) for num in range(START, END + 1)]
         
-        clear_screen()
         print(f"Extracting images from {START} - {END}")
         
         for future in futures:
